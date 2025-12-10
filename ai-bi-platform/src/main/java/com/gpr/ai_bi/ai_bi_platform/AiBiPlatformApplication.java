1174636bd7653a2703b0d09cd98e263aa1417e83
tanakahmm <gurramprajith@gmail.com>
2025-12-10 13:12:44 +0530

    adding springboot initialiser to setup backend

---
+++ ai-bi-platform/src/main/java/com/gpr/ai_bi/ai_bi_platform/AiBiPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.gpr.ai_bi.ai_bi_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiBiPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiBiPlatformApplication.class, args);
+	}
+
+}
---
+++ ai-bi-platform/src/test/java/com/gpr/ai_bi/ai_bi_platform/AiBiPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpr.ai_bi.ai_bi_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiBiPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
